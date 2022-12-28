0f269f78601af35a1f8362c8155709007862f042
Luciano Bitencourt <lucbite@gmail.com>
2022-12-28 10:24:50 -0300

    .
    ;

---
+++ js/parimp.js
@@ -0,0 +1,16 @@
+
+ 
+    function parimp(n){
+      if (n % 2 == 0) {
+        return  'par'
+      } else if (n % 2 != 0) {
+        return 'impar'
+      }
+    }
+
+    let valor = parimp(prompt())
+    console.log(valor);
+    alert(valor)
+    
+
+
